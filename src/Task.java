e89df7b782e1c3a8e65e808927e853584c8fc7f1
MarcinM <mircin27@gmail.com>
2023-10-19 17:11:26 +0200

    added zadania

---
+++ src/Task.java
@@ -0,0 +1,65 @@
+public class Task {
+    /*
+
+    1.Poczytać co to jest złożoność obliczeniowa (do czego to służy, jakie są jej rodzaje, jak można to policzyć).
+2.Napisz program generujący jednowymiarową tablicę losowych liczb (10 elementów) w której każda liczba jest podzielna przez 7. Wynikiem działania programu ma być wyświetlona tablica. Zakres losowych liczb 0 - 100.
+3.Napisz program generujący dwuwymiarową tablice (5x5) losowych liczb. Wynikiem działania programu ma być wyświetlona tablica oraz najmniejsza liczba z tej tablicy. Zakres losowych liczb 0 - 100.
+4.Napisz program generujący jednowymiarową tablicę (10 elementów) liczb w której każda liczba jest losowa ale nie jest mniejsza niż poprzednia liczba. Przykłady:
+23, 34, 53, 60, 80
+0, 10, 70, 70, 70
+nieprawidłowy wynik:
+34, 56, 34, 78, 99 - trzecia liczba jest mniejsza niż poprzednia (druga).
+Wynikiem działania programu ma być wyświetlona wygenerowana tablica. Zakres losowych liczb 0 - 20.
+5.Napisz program w którym jest zmienna 'w'. Zmienna ta oznacza wysokość choinki. Program wyświetla na ekranie wypisaną choinkę o danej wysokości. Przykłady:
+dla w = 3 program wypisuje
+  *
+ ***
+*****
+pierwszy wiersz to: spacja spacja gwiazdka spacja spacja
+drugi wiersz to: spacja gwiazdka gwiazdka gwiazdka spacja
+trzeci wiersz to: gwiazdka gwiazdka gwiazdka gwiazdka gwiazdka
+dla w = 5
+    *
+   ***
+  *****
+ *******
+*********
+6.Napisz program który generuje jednowymiarową tablicę (10 elementów) losowych liczb. Wynikiem działania programu ma być wygenerowana tablica oraz ilość powtórzeń liczby 5. Zakres losowych liczb 0 - 10.
+7.Napisz program który generuje jednowymiarową tablicę (10 elementów) losowych liczb. Program ma wygenerować druga tablicę która jest pierwszą tablicą w odwrotnej kolejności. Wynikiem działania programu ma być wyświetlona pierwsza i druga tablica. Przykłady:
+3, 23, 2, 23, 53
+53, 23, 2, 23, 345, 23, 65, 1, 54
+54, 1, 65, 23, 45
+Zakres losowych liczb 0 - 100.
+8.Napisz program który generuje dwie tablice (10 elementów) losowych liczb. Wynikiem działania programu mają być wyświetlone obie tablice oraz trzecia tablica będąca połączeniem dwóch wygenerowanych. Przykład:
+3, 5, 12, 34, 45
+45, 21, 65, 23, 1
+3, 5, 12, 34, 45, 45, 21, 65, 23, 1
+9.Napisz program który losuje dwie tablice (10 elementów, zakres 0 - 100). Wynikiem działania programu mają być wypisane obie tablice, oraz trzecia tablica w które każdy element jest sumą odpowiadających elementów z dwóch pierwszych tablic. Przykład:
+1 4 4 64 45
+23 32 5 23 3
+24 36 9 87 48
+10.Napisac program w którym są 3 zmienne:
+s - liczba całkowita - oznacza pierwszą liczbę ciągu arytmetycznego
+k - liczba całkowita - oznacza krok w ciągu arytmetycznym
+n - liczba całkowita - oznacza ilość elementów ciągu.
+Program ma zwracać sumę elementów takiego ciągu. Przykład:
+s = 3, k = 5, n = 4
+program ma wygenerować 4 elementy (n), które wyglądają dla podanych s i k tak:
+3, 8, 13, 18
+następnie je sumuje i podaje wynik: 42.
+Info o tym jak działa ciąg arytmetyczny jest na wikipedii :)
+11.Napisz program który ma 3 zmienna a, b i c. Te zmienne to długości boków trójkąta. Program ma sprawdzić czy z podanych boków da się zbudować trójkąt. Jeśli się da to program ma wypisać na ekranie true (w przeciwnym wypadku false) i obliczyć obwód tego trójkąta. Jak sprawdzić czy z podanych boków da się zbudować trójkąt znajdziecie w google pod hasłem: "Nierówność trójkąta".
+12.Napisz program który ma dwie zmienne:
+weight - waga, liczba całkowita
+height - wzrost, liczba całkowita
+program ma obliczać BMI (wskaźnik masy ciała, wzór do znalezienia w internecie, gdybyście nie znaleźli to piszcie) i na podstawie wyliczonego BMI wypisywać "Nadwaga, Niedowaga, W normie itp.
+13.Napisz program który symuluje rzuty dwiema kastkami do gry (takie zwykłe białe kości jak w monopoly). Program ma wykonać 10 000 rzutów i wypisać liczbę która wypadała najczęściej (ta liczba jest oczywiście sumą oczek na dwóch kościach które symulujemy.
+14.Napisz program który generuje tablicę liczb 0 i 1 (10 elementów). Program ma sprawdzić czy tablica jest symetryczna - pierwszy element taki jak ostatni, drugi taki jak przedostatni itd. Wynikiem działania programu ma być wypisana wygenerowana tablica oraz true albo false w zależności czy jest symetria czy nie. (edited)
+   ReactReply
+
+
+
+
+
+    * */
+}
